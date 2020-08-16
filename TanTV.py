7ea15fc4ad8d6d7783da4dac6f90ad277da976c7
egormanga <egorka000111000@gmail.com>
2020-08-16 19:10:32 +0300

    Initial commit

---
+++ TanTV.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# TanTV
+
+import http.server
+from utils import *; logstart('TanTV')
+
+PORT = 7133
+
+channels = json.load(open('channels.json'))
+
+channel_rr = Sdict(int)
+
+class TanTVHandler(http.server.BaseHTTPRequestHandler):
+	def do_GET(self):
+		global channel_rr
+
+		if (self.path == '/list.m3u'):
+			self.send_response(200)
+			self.end_headers()
+			self.wfile.write(('#EXTM3U\n'+'\n'.join(f"#EXTINF:-1,{i}\nhttp://{socket.gethostbyname(socket.gethostname())}:{self.server.server_port}/{ii+1}" for ii, i in enumerate(channels))).encode('utf8'))
+			return
+
+		ch = self.path.strip('/')
+
+		if (not ch.isdigit() or int(ch) > len(channels)):
+			self.send_error(404, explain=f"No such channel: {ch}")
+			return
+
+		ch = int(ch)
+		urls = channels[tuple(channels.keys())[ch-1]]
+		rri = channel_rr[ch-1]
+		for i in (urls*2)[rri:rri+len(urls)]:
+			try:
+				r = requests.get(i, stream=True, timeout=(1, 1))
+				if (r.ok and r.raw.read(1)): break
+			except (requests.exceptions.RequestException, urllib3.exceptions.ReadTimeoutError): pass
+			channel_rr[ch-1] = (rri+1) % len(urls)
+		else:
+			self.send_error(408)
+			channel_rr[ch-1] = 0
+			return
+
+		self.send_response(200)
+
+		for k, v in r.headers.items():
+			self.send_header(k, v)
+		self.end_headers()
+
+		try:
+			for chunk in r:
+				self.wfile.write(chunk)
+		except OSError: pass
+
+def main():
+	s = http.server.ThreadingHTTPServer(('', PORT), TanTVHandler)
+	try: s.serve_forever()
+	except KeyboardInterrupt as ex: exit(ex)
+
+if (__name__ == '__main__'): logstarted(); exit(main())
+else: logimported()
+
+# by Sdore, 2020
---
+++ __main__.py
@@ -0,0 +1,1 @@
+TanTV.py
